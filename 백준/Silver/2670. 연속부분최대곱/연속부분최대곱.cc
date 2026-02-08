30c0ebf7ca0703e234a0abf9a85746fda23f7d70
WooSeok Choi <ddakgi00@naver.com>
2026-02-08 16:20:35 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <stack>
 #include <vector>
 #include <algorithm>
 
@@ -10,6 +12,11 @@ using namespace std;
 * 그런데 이 문제는 연속성을 요구한다는 것이 문제..
 * 
 * 그냥 브루트포스로 풀었더니 풀리네?
+* 그러면 n이 증가하면 어떻게 풀어야 해? -> DP를 떠올리긴 했는데 내가 DP를 잘 모름
+* 교안에서는 그리디를 사용함
+* 과거의 누적된 값 * 현재값이 현재값보다 크면 현재값을 곱해서 갱신
+* 과거의 누적된 값 * 현재값이 현재값보다 작으면 값을 현재값으로 갱신
+* O(n)의 그리디 알고리즘
 * 
 */
 
@@ -27,15 +34,14 @@ int main() {
 	for (int i = 0; i < n; i++) cin >> v[i];
 
 	double result = 0;
+	double temp;
 	for (int i = 0; i < n; i++) {
-		double temp = v[i];
-		result = max(result, v[i]);
-		for (int j = i + 1; j < n; j++) {
-			result = max(result, temp * v[j]);
-			//cout << temp * v[j] << " ";
-			temp *= v[j];
+		if (i == 0) temp = v[i];
+		else {
+			if (v[i] > temp * v[i]) temp = v[i];
+			else temp *= v[i];
 		}
-		//cout << '\n';
+		result = max(result, temp);
 	}
 	cout << result;
 }
