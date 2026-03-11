9bfc6cf8c91cc7dbb9500dff6fbd1bea98192a26
WooSeok Choi <ddakgi00@naver.com>
2026-03-12 07:21:17 +0900

    [Silver V] Title: 운동회, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Silver/25425. 운동회/운동회.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+
+/*
+* 25425. 운동회
+* N팀이 m명의 인원으로 구성
+* 라운드당 한명 탈락, 팀의 모든 인원 탈락 시 팀 탈락
+* 자신의 팀이 가질 수 있는 범위를 계산해라
+* 팀이 가질 수 있는 등수는 남아있는 팀의 수 + 1
+*/
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int n, m; cin >> n >> m;
+	int a, k; cin >> a >> k;
+
+	int min, max;
+	min = n - ((n * m) - a) / m;
+	if (a < n) max = (a - k) + 1;
+	else max = n;
+
+	cout << max << " " << min;
+
+
+}
