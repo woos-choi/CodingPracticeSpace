2b5a6f9a8703a2a8b4040bf6a68ecd9d0d15fb95
WooSeok Choi <ddakgi00@naver.com>
2026-03-14 11:58:58 +0900

    [Silver V] Title: 운동회, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

--- 백준/Silver/25425. 운동회/운동회.cc
+++ 백준/Silver/25425. 운동회/운동회.cc
@@ -1,48 +1,35 @@
 #include <iostream>
-#include <climits>
 #include <queue>
 #include <vector>
+#include <climits>
+#include <string>
 #include <algorithm>
-#include <math.h>
 
 using namespace std;
 
-
-/*
-* 25425. 운동회
-* N팀이 m명의 인원으로 구성
-* 라운드당 한명 탈락, 팀의 모든 인원 탈락 시 팀 탈락
-* 자신의 팀이 가질 수 있는 범위를 계산해라
-* 팀이 가질 수 있는 등수는 남아있는 팀의 수 + 1
-*/
-
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	
-	int n, m; cin >> n >> m;
-	int a, k; cin >> a >> k;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-	int min, max;
-	
-	int temp = a - k; //현재 다른 팀에 남아 있는 인원 수
-	
-	if (temp >= n) {
-		max = n;
-		min = temp / m;
-		if (temp % m > 0) min += 1;
-		min += 1;
-	}
+    int n, m, a,k;
+    cin >> n >> m >> a >> k;
 
-	else {
-		max = temp + 1;
-		min = temp / m;
-		if (temp % m > 0) min += 1;
-		min += 1;
-	}
+    int temp = a - k;
+    //현재 남은 인원수를 temp라고 하자 (우리 팀은 제외하고)
+    //지금 남음 팀원 수는
+    int min, max;
+    min = temp / m + 1;
+    if (temp % m != 0) min += 1;
 
-	cout << max << " " << min;
+    if (temp >= n) max = 30;
+    else max = temp + 1;
+
+    cout << max << " " << min;
+
+
 
 
 }
+
+
+
