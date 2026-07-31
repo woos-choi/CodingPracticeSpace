c554435f90e528603849c5e18846bcde43ce4c3b
WooSeok Choi <ddakgi00@naver.com>
2026-07-31 12:45:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.73 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,17 +1,23 @@
+/*
+1. 최단거리니까 BFS
+2. 격자는 끽해봐야 100*100
+*/
 import java.util.*;
 
 class Solution {
+    int[] dr = {-1,1,0,0};
+    int[] dc = {0,0,-1,1};
     
-    int[] dr = {-1, 1, 0, 0};
-    int[] dc = {0, 0, -1, 1};
-    
-    boolean[][] visited;
+    int n, m;
+    int answer = -1;
+    int[][] maps;
     Deque<int[]> q = new ArrayDeque<>();
-    int answer;
+    boolean [][] visited;
     
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
+        this.maps = maps;
+        n = maps.length;
+        m = maps[0].length;
         visited = new boolean[n][m];
         
         q.offer(new int[]{0,0,1});
@@ -19,9 +25,11 @@ class Solution {
         
         while(!q.isEmpty()){
             int[] node = q.poll();
-            int r = node[0], c = node[1], dist = node[2];
+            int r = node[0], c = node[1], distance = node[2];
             
-            if(r == n-1 && c == m-1) return dist;
+            if(r == n-1 && c == m-1)  answer = node[2];
+               
+        
             
             for(int i=0; i<4; i++){
                 int nr = r + dr[i];
@@ -32,10 +40,11 @@ class Solution {
                 if(maps[nr][nc] == 0) continue;
                 
                 visited[nr][nc] = true;
-                q.offer(new int[]{nr, nc, dist + 1});
+                q.offer(new int[]{nr,nc,distance + 1});
             }
         }
- 
-        return -1;
+        
+        System.out.println(answer);
+        return answer;
     }
 }
