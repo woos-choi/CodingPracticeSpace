c2ebac97a4bf3df4eefe5c782105e471be7e4358
WooSeok Choi <ddakgi00@naver.com>
2026-07-31 13:03:17 +0900

    [level 2] Title: 타겟 넘버, Time: 5.13 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,33 @@
 import java.util.*;
 
+/*
+순열 백트래킹이라는걸 인지하고 들어갈 것
+백트래킹도 결국 DFS의 일종임을 계속 인지하고 있을 것
+*/
 
 class Solution {
+    int answer;
     int[] numbers;
     int target;
-    int answer = 0;
-    
+    int n;
     
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
+        n = numbers.length;
         
-        dfs(0,0);
+        DFS(0,0);
         
         return answer;
     }
     
-    public void dfs(int index, int sum){
-        if(index == numbers.length){
+    public void DFS(int depth, int sum){
+        if(depth == n){
             if(sum == target) answer++;
             return;
         }
         
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
+        DFS(depth + 1, sum + numbers[depth]);
+        DFS(depth + 1, sum - numbers[depth]);
     }
 }
